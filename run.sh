b147960d5f4f782618736fb8b0ecb107d50b0b5f
thomascastroneto <thomascastroneto@gmail.com>
2022-09-21 23:19:19 +0000

    ee

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "start cs144 buffer bloat experiment"
-sudo sysctl -w net.ipv4.tcp_congestion_control=reno
+sudo sysctl -w net.ipv4.tcp_congestion_control=bbr
 sudo sysctl -w net.ipv4.tcp_min_tso_segs=1
 python bufferbloat.py --bw-host 1000 \
                 --bw-net 1.5 \
